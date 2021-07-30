04430b59a00ad5ef8999cbc06914ce5c0aedebac
Sauceke <76826783+Sauceke@users.noreply.github.com>
2021-07-30 08:50:25 +0000

    Bump version

--- ButtPlugin.Core/CoreConfig.cs
+++ ButtPlugin.Core/CoreConfig.cs
@@ -7,7 +7,7 @@ namespace ButtPlugin.Core
     public class CoreConfig
     {
         public const string GUID = "Sauceke.ButtPlugin";
-        public const string Version = "1.2.0";
+        public const string Version = "2.0.0";
 
         public static ManualLogSource Logger { get; set; }
         public static PluginInfo Info { get; internal set; }
