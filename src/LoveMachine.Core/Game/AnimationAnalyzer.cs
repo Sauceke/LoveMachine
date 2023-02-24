78dc6358437f2db303c8c9cff6c810a858a5ba93
Sauceke <76826783+Sauceke@users.noreply.github.com>
2023-02-24 10:16:51 +0300

    cleanup

--- src/LoveMachine.Core/Game/AnimationAnalyzer.cs
+++ src/LoveMachine.Core/Game/AnimationAnalyzer.cs
@@ -4,9 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
 using UnityEngine;
-using UnityEngine.Profiling;
 
 namespace LoveMachine.Core
 {
