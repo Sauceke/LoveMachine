4d5e5ef4852b5443d280d9fcfb7236549ddb0968
Sauceke <76826783+Sauceke@users.noreply.github.com>
2023-05-09 14:54:32 +0300

    add sch

---
+++ src/LoveMachine.SCH/Plugin.cs
@@ -0,0 +1,8 @@
+﻿using BepInEx;
+using LoveMachine.Core;
+
+namespace LoveMachine.SCH;
+
+[BepInPlugin(CoreConfig.GUID, CoreConfig.PluginName, CoreConfig.Version)]
+internal class Plugin : LoveMachinePlugin<SolasCityHeroesGame>
+{ }
---
+++ src/LoveMachine.SCH/SolasCityHeroesGame.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Reflection;
+using HarmonyLib;
+using LoveMachine.Core;
+using UnityEngine;
+
+namespace LoveMachine.SCH;
+
+public class SolasCityHeroesGame : GameDescriptor
+{
+    private Traverse sexSystem;
+    
+    protected override MethodInfo[] StartHMethods =>
+        new[] { AccessTools.Method("SexSystem, Assembly-CSharp:StartSex") };
+    
+    protected override MethodInfo[] EndHMethods => new[]
+    {
+        AccessTools.Method("SexSystem, Assembly-CSharp:Escape"),
+        AccessTools.Method("PlayerHub, Assembly-CSharp:Start")
+    };
+
+    protected override Dictionary<Bone, string> FemaleBoneNames => new()
+    {
+        { Bone.Vagina, "groin_R" },
+        { Bone.Mouth, "MouthLower" },
+        { Bone.LeftHand, "hand_L" },
+        { Bone.RightHand, "hand_R" },
+    };
+
+    protected override Transform PenisBase => throw new NotImplementedException();
+
+    protected override Transform[] PenisBases => new[]
+    {
+        GameObject.Find("Enemy/ArmatureFem_000/Global/Position/waist/groin_R").transform,
+        GameObject.Find("Enemy/ArmatureFem_000/Global/Position/waist/stomach/torso/neck/head/MouthLower").transform,
+        GameObject.Find("Enemy/ArmatureFem_000/Global/Position/waist/stomach/torso/collar_L/shoulder_L/arm_L/hand_L").transform,
+        GameObject.Find("Enemy/ArmatureFem_000/Global/Position/waist/stomach/torso/collar_R/shoulder_R/arm_R/hand_R").transform
+    };
+    
+    protected override int AnimationLayer => 0;
+    
+    protected override int HeroineCount => 1;
+    
+    protected override int MaxHeroineCount => 1;
+    
+    protected override bool IsHardSex => true;
+
+    protected override Animator GetFemaleAnimator(int girlIndex) =>
+        sexSystem.Property<Animator>("PlayerAnim").Value;
+    
+    protected override GameObject GetFemaleRoot(int girlIndex) => GameObject.Find("/Player");
+
+    protected override string GetPose(int girlIndex) =>
+        GetFemaleAnimator(0).GetCurrentAnimatorStateInfo(0).fullPathHash.ToString();
+
+    protected override bool IsIdle(int girlIndex) => Time.timeScale == 0f;
+
+    protected override void SetStartHInstance(object instance) =>
+        sexSystem = Traverse.Create(instance);
+    
+    protected override IEnumerator UntilReady()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+    }
+}
