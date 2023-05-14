e35d923b7db9af06b6ef025f0949015eb8492bce
Sauceke <76826783+Sauceke@users.noreply.github.com>
2023-05-14 20:33:00 +0300

    Exclude il2cpp projects from assemblyalias

--- src/alias-deps.bat
+++
@@ -1,1 +0,0 @@
-assemblyalias --target-directory %1 --suffix _Of_Love --assemblies-to-alias "LitJSON;SuperSocket.ClientEngine;WebSocket4Net"
---
+++ src/alias-deps.ps1
@@ -0,0 +1,8 @@
+cd $PSScriptRoot
+$plugins = Get-ChildItem -Name -Path ./bin/ | Where-Object { $_ -notlike "LoveMachine.Core*" }
+foreach ($plugin in $plugins) {
+	if (Test-Path "./bin/$plugin/BepInEx/plugins/*/LoveMachine.Core.IL2CPP.dll") {
+		continue
+	}
+	assemblyalias --target-directory "./bin/$plugin/BepInEx/plugins" --suffix _Of_Love --assemblies-to-alias "LitJSON;SuperSocket.ClientEngine;WebSocket4Net"
+}
