68f95b37fcaa9e20316337df53436a875eb37328
Sauceke <76826783+Sauceke@users.noreply.github.com>
2023-05-14 20:44:35 +0300

    install alias in script instead of release job

--- src/alias-deps.ps1
+++ src/alias-deps.ps1
@@ -1,4 +1,5 @@
 cd $PSScriptRoot
+dotnet tool install --global Alias
 $plugins = Get-ChildItem -Name -Path ./bin/ | Where-Object { $_ -notlike "LoveMachine.Core*" }
 foreach ($plugin in $plugins) {
 	if (Test-Path "./bin/$plugin/BepInEx/plugins/*/LoveMachine.Core.IL2CPP.dll") {
