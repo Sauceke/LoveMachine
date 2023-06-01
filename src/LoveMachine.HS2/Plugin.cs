7258f87952d3bee2c56be48e805a6188c413f96b
Sauceke <76826783+Sauceke@users.noreply.github.com>
2023-06-01 22:08:25 +0300

    [HS2] Add slap logic

--- src/LoveMachine.HS2/Plugin.cs
+++ src/LoveMachine.HS2/Plugin.cs
@@ -7,7 +7,13 @@ namespace LoveMachine.HS2
     [BepInProcess("HoneySelect2VR")]
     [BepInPlugin(Globals.GUID, Globals.PluginName, Globals.Version)]
     internal class Plugin : LoveMachinePlugin<HoneySelect2Game>
-    { }
+    {
+        protected override void Start()
+        {
+            base.Start();
+            Globals.ManagerObject.AddComponent<SlapGimmick>();
+        }
+    }
 
     // To avoid conflict with the old plugin
     // 2.1.0 was the last ButtPlugin version so this should get priority from BepInEx
---
+++ src/LoveMachine.HS2/SlapGimmick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Linq;
+using LoveMachine.Core;
+using UnityEngine;
+
+namespace LoveMachine.HS2
+{
+    public class SlapGimmick : Gimmick
+    {
+        private readonly string[] slapAnimationNames = { "SAction", "WAction" };
+        
+        protected override IEnumerator Run(Device device)
+        {
+            var hs2 = GetComponent<HoneySelect2Game>();
+            float updateTimeSecs = 1f / device.Settings.UpdatesHz;
+            while (true)
+            {
+                yield return new WaitForSecondsRealtime(updateTimeSecs);
+                var info = hs2.animators[device.Settings.GirlIndex]
+                    .GetCurrentAnimatorStateInfo(0);
+                if (slapAnimationNames.Any(info.IsName))
+                {
+                    yield return DoStroke(device, info.length);
+                    yield return new WaitForSecondsRealtime(info.length / 2);
+                }
+            }
+        }
+    }
+}
