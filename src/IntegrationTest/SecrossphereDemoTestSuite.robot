f862a0eba2f39e5c9cde89af9fce87c91f0273a1
Sauceke <saucekedesu@protonmail.com>
2023-06-23 19:47:52 +0300

    Add robotframework test suite

---
+++ src/IntegrationTest/SecrossphereDemoTestSuite.robot
@@ -0,0 +1,30 @@
+*** Settings ***
+Library           LoveMachineLibrary.py
+Suite Setup       Play The Game
+Suite Teardown    Clean Up
+
+*** Test Cases ***
+Check Linear Commands
+    Number Of Linear Commands Should Be At Least    ${10}
+    Milliseconds Between Linear Commands Should Be About    ${400}
+    Positions Of Linear Commands Should Alternate
+
+Check Vibrate Commands
+    Milliseconds Between Vibrate Commands Should Be About    ${100}
+
+*** Keywords ***
+Play The Game
+    Download Secrossphere Demo
+    Patch Secrossphere Demo
+    Set Secrossphere Resolution    ${320}    ${240}    ${1}
+    Use Secrossphere Config    ./scs-config
+    Start Fake Intiface Server
+    Start Secrossphere Demo
+    Start H Scene In Secrossphere Demo
+    Sleep   20 seconds
+
+Clean Up
+    Close Secrossphere Demo
+    Stop Fake Intiface Server
+    Sleep    5 seconds
+    Delete Downloaded Files
---
+++ src/IntegrationTest/intifake.py
@@ -0,0 +1,117 @@
+import concurrent
+import json
+import asyncio
+import time
+import websockets
+import threading
+
+# Fake Intiface server using example responses taken straight from the Buttplug docs.
+
+server_info = """
+[
+    {
+        "ServerInfo": {
+            "Id": 1,
+            "ServerName": "Test Server",
+            "MessageVersion": 1,
+            "MaxPingTime": 100
+        }
+    }
+]
+"""
+
+device_list = """
+[
+    {
+        "DeviceList": {
+            "Id": 1,
+            "Devices": [
+                {
+                    "DeviceName": "Test Vibrator",
+                    "DeviceIndex": 0,
+                    "DeviceMessages": {
+                        "ScalarCmd": [
+                            {
+                                "StepCount": 20,
+                                "FeatureDescriptor": "Clitoral Stimulator",
+                                "ActuatorType": "Vibrate"
+                            },
+                            {
+                                "StepCount": 20,
+                                "FeatureDescriptor": "Insertable Vibrator",
+                                "ActuatorType": "Vibrate"
+                            }
+                        ],
+                        "StopDeviceCmd": {}
+                    }
+                },
+                {
+                    "DeviceName": "Test Stroker",
+                    "DeviceIndex": 1,
+                    "DeviceMessageTimingGap": 100,
+                    "DeviceDisplayName": "User set name",
+                    "DeviceMessages": {
+                        "LinearCmd": [{
+                            "StepCount": 100,
+                            "FeatureDescriptor": "Stroker",
+                            "ActuatorType": "Linear"
+                        }],
+                        "StopDeviceCmd": {}
+                    }
+                }
+            ]
+        }
+    }
+]
+"""
+
+linear_commands = {}
+vibrate_commands = {}
+
+async def handle(websocket):
+    while True:
+        try:
+            message = await websocket.recv()
+        except:
+            print("recv error, assuming connection closed")
+            stop_handle.set_result(0)
+            continue
+        obj = json.loads(message)[0]
+        if "RequestServerInfo" in obj:
+            await websocket.send(server_info)
+            continue
+        if "RequestDeviceList" in obj:
+            await websocket.send(device_list)
+            continue
+        if "LinearCmd" in obj:
+            global linear_commands
+            linear_commands[time.time()] = message
+            continue
+        if "ScalarCmd" in obj:
+            global vibrate_commands
+            vibrate_commands[time.time()] = message
+            continue
+
+async def run_loop():
+    global stop_handle
+    stop_handle = asyncio.Future()
+    server = await websockets.serve(handle, "localhost", 12345)
+    await stop_handle
+    try:
+        await server.close()
+    except:
+        pass
+
+def run_fg():
+    asyncio.run(run_loop())
+
+def start():
+    global server_thread
+    server_thread = threading.Thread(target = run_fg)
+    server_thread.start()
+
+def stop():
+    global stop_handle
+    stop_handle.set_result(0)
+    global server_thread
+    server_thread.join()
