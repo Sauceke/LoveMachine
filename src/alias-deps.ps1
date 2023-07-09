1feaa0bb63d30224187c7bb3ee3272aa36ce2915
Sauceke <saucekedesu@protonmail.com>
2023-07-09 12:26:32 +0300

    Find dependencies to alias automatically

--- src/alias-deps.ps1
+++ src/alias-deps.ps1
@@ -3,7 +3,15 @@ dotnet tool install --global Alias
 $plugins = Get-ChildItem -Name -Path ./bin/ | Where-Object { $_ -notlike "LoveMachine.Core*" }
 foreach ($plugin in $plugins) {
 	if (Test-Path "./bin/$plugin/BepInEx/plugins/*/LoveMachine.Core.IL2CPP.dll") {
+		# don't alias il2cpp plugins
 		continue
 	}
-	assemblyalias --target-directory "./bin/$plugin/BepInEx/plugins" --suffix _Of_Love --assemblies-to-alias "LitJSON;SuperSocket.ClientEngine;WebSocket4Net"
+	$suffix = "_Of_Love"
+	$path = "./bin/$plugin/BepInEx/plugins"
+	# find all dependencies to alias
+	$dlls = Get-ChildItem $path -Filter "*.dll" -Recurse
+	$dll_names = $dlls -Replace "\.dll$",""
+	$deps = $dll_names | Where { $_ -NotLike "LoveMachine.*" -And $_ -NotLike "*$suffix" }
+	$deps_str = $deps -Join ";"
+	assemblyalias --target-directory $path --suffix $suffix --assemblies-to-alias $deps_str 
 }
