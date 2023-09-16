d6accd1b1eb2f67e9639f091218912517c43722c
Sauceke <saucekedesu@protonmail.com>
2023-09-16 20:28:48 +0300

    [COM3D2] cache player object

--- src/LoveMachine.COM3D2/Com3d2Game.cs
+++ src/LoveMachine.COM3D2/Com3d2Game.cs
@@ -19,28 +19,20 @@ namespace LoveMachine.COM3D2
         private readonly string[] climaxPoseNames = { "shasei_", "zeccyou_" };
 
         private TimeUnlooper unlooper;
+        private GameObject player;
 
         protected override Dictionary<Bone, string> FemaleBoneNames => new Dictionary<Bone, string>
         {
-            { Bone.Vagina, PelvisF + "/_IK_vagina" },
+            { Bone.Vagina, $"{PelvisF}/_IK_vagina" },
+            { Bone.Mouth, $"{SpineF}/Bip01 Neck/Bip01 Head/_SM_face007/MouthUp" },
             {
                 Bone.RightHand,
-                SpineF + "/Bip01 R Clavicle/Bip01 R UpperArm/Bip01 R Forearm/Bip01 R Hand"
+                $"{SpineF}/Bip01 R Clavicle/Bip01 R UpperArm/Bip01 R Forearm/Bip01 R Hand"
             },
-            {
-                Bone.Mouth,
-                SpineF + "/Bip01 Neck/Bip01 Head/_SM_face007/MouthUp"
-            },
-            { Bone.LeftBreast, SpineF + "/Mune_L/_IK_muneL" },
-            { Bone.RightBreast, SpineF + "/Mune_R/_IK_muneR" },
-            {
-                Bone.RightFoot,
-                PelvisF +  "/Bip01 L Thigh/Bip01 L Calf/Bip01 L Foot/Bip01 L Toe0"
-            },
-            {
-                Bone.LeftFoot,
-                PelvisF +  "/Bip01 R Thigh/Bip01 R Calf/Bip01 R Foot/Bip01 R Toe0"
-            }
+            { Bone.LeftFoot, $"{PelvisF}/Bip01 R Thigh/Bip01 R Calf/Bip01 R Foot/Bip01 R Toe0" },
+            { Bone.RightFoot, $"{PelvisF}/Bip01 L Thigh/Bip01 L Calf/Bip01 L Foot/Bip01 L Toe0" },
+            { Bone.LeftBreast, $"{SpineF}/Mune_L/_IK_muneL" },
+            { Bone.RightBreast, $"{SpineF}/Mune_R/_IK_muneR" }
         };
 
         protected override int HeroineCount => FindCharaObject("Maid[1]") == null ? 1 : 2;
@@ -77,7 +69,7 @@ namespace LoveMachine.COM3D2
             speed = state.speed;
         }
 
-        private AnimationState GetActiveState() => FindCharaObject("Man[0]")
+        private AnimationState GetActiveState() => player
             .GetComponentsInChildren<Animation>()
             .SelectMany(animation => animation.Cast<AnimationState>()
                 .Where(state => animation.IsPlaying(state.name)))
@@ -88,8 +80,8 @@ namespace LoveMachine.COM3D2
         protected override GameObject GetFemaleRoot(int girlIndex) =>
             FindCharaObject($"Maid[{girlIndex}]");
 
-        protected override Transform PenisBase => FindBoneByPath(FindCharaObject("Man[0]"),
-            "ManBip/ManBip Pelvis/chinkoCenter/tamabukuro");
+        protected override Transform PenisBase =>
+            FindBoneByPath(player, "ManBip/ManBip Pelvis/chinkoCenter/tamabukuro");
 
         private static GameObject FindCharaObject(string pattern) =>
             GameObject.Find("__GameMain__/Character/Active/AllOffset").transform.Cast<Transform>()
@@ -110,6 +102,7 @@ namespace LoveMachine.COM3D2
         {
             yield return new WaitForSeconds(5f);
             unlooper = new TimeUnlooper();
+            player = FindCharaObject("Man[0]");
         }
     }
 }
