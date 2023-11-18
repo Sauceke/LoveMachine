a8e01cb16fedcd51aaa84fd8a9bcf5b3524c9b71
Sauceke <saucekedesu@protonmail.com>
2023-11-18 21:48:48 +0300

    [HKR] tentacles + use multi-penis feature

--- src/LoveMachine.HKR/HolyKnightRiccaGame.cs
+++ src/LoveMachine.HKR/HolyKnightRiccaGame.cs
@@ -10,11 +10,14 @@ namespace LoveMachine.HKR;
 
 internal class HolyKnightRiccaGame : TimelineGameAdapter
 {
+    private GameObject SceneRoot => GameObject.Find("ATDTimeline");
+    
     private static readonly string[] dickBasePaths =
     {
         "DEF-testicle",
         "ORG-testicle",
         "DEF-Ovipositor",
+        "mainTentacle03_013",
         "hellbeetle/Base/belly/belly_001/belly_002/tail"
     };
 
@@ -30,6 +33,13 @@ internal class HolyKnightRiccaGame : TimelineGameAdapter
         AccessTools.Method("ATD.UIController, ATDAssemblyDifinition:OnDestroy")
     };
     
+    protected override Traverse Director => Traverse.Create(FindObjectOfType<PlayableDirector>());
+
+    protected override Traverse Timeline =>
+        Traverse.Create(FindObjectOfType<PlayableDirector>().playableAsset.Cast<TimelineAsset>());
+    
+    protected override string TrackName => "Cut Track Asset";
+    
     protected override Dictionary<Bone, string> FemaleBoneNames => new()
     {
         { Bone.Vagina, "DEF-clitoris" },
@@ -37,27 +47,16 @@ internal class HolyKnightRiccaGame : TimelineGameAdapter
     };
 
     protected override int HeroineCount => 1;
-
     protected override int MaxHeroineCount => 1;
-
     protected override bool IsHardSex => true;
-
     protected override float PenisSize => 0.1f;
+    protected override Transform PenisBase => throw new NotImplementedException();
 
-    protected override Transform PenisBase => dickBasePaths
-        .Select(GameObject.Find)
-        .First(go => go != null)
-        .transform;
+    protected override Transform[] PenisBases => dickBasePaths
+        .SelectMany(path => FindDeepChildrenByPath(SceneRoot, path))
+        .ToArray();
 
-    protected override GameObject GetFemaleRoot(int girlIndex) =>
-        GameObject.Find("ricasso/root");
+    protected override GameObject GetFemaleRoot(int girlIndex) => GameObject.Find("ricasso/root");
 
     protected override bool IsIdle(int girlIndex) => false;
-
-    protected override Traverse Director => Traverse.Create(FindObjectOfType<PlayableDirector>());
-
-    protected override Traverse Timeline =>
-        Traverse.Create(FindObjectOfType<PlayableDirector>().playableAsset.Cast<TimelineAsset>());
-    
-    protected override string TrackName => "Cut Track Asset";
 }
