2d990bc132ddf4a2977e4da1e910cbadffcb30b4
Sauceke <saucekedesu@protonmail.com>
2025-04-23 19:28:50 +0300

    [Core] Only optimize subdivisions on sine curve

--- src/LoveMachine.Core/Controller/StrokerController.cs
+++ src/LoveMachine.Core/Controller/StrokerController.cs
@@ -22,8 +22,10 @@ namespace LoveMachine.Core.Controller
             float durationSecs = strokeInfo.DurationSecs;
             // max number of subdivisions given the update frequency
             int subdivisions = 2 * (int)Mathf.Max(1f, durationSecs * updateFrequency / 2);
-            // 4 subdivisions is mathematically the same as 2
-            subdivisions = subdivisions == 4 ? 2 : subdivisions;
+            // on a sine curve, 4 linear segments is mathematically the same as 2
+            subdivisions = (settings.Pattern == StrokingPattern.Sine && subdivisions == 4)
+                ? 2
+                : subdivisions;
             int segments = settings.SmoothStroking ? subdivisions : 2;
             float startCompletion = strokeInfo.Completion;
             float nextSegmentCompletion = Mathf.Round(startCompletion * segments + 1) / segments;
