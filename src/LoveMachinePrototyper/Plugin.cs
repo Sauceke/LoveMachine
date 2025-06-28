e99a1545a320bcdf7b3aad7879e33949e8185750
Sauceke <saucekedesu@protonmail.com>
2025-06-29 00:44:28 +0300

    [Prototyper] Refactor

--- src/LoveMachinePrototyper/Plugin.cs
+++ src/LoveMachinePrototyper/Plugin.cs
@@ -12,12 +12,12 @@ namespace LoveMachinePrototyper
             base.Start();
             PrototyperConfig.Initialize(this);
             Globals.ManagerObject.AddComponent<HListener>();
-            if (PrototyperConfig.PluginVersion.Value != Globals.Version)
+            if (PrototyperConfig.PluginVersion.Value != PrototyperConfig.PluginVersion.DefaultValue)
             {
                 Logger.LogWarning("This config file was written in LoveMachine Prototyping Tool " +
                     $"{PrototyperConfig.PluginVersion.Value}, and may not work in other versions.");
             }
-            if (PrototyperConfig.GameProcessName.Value != Paths.ProcessName)
+            if (PrototyperConfig.GameProcessName.Value != PrototyperConfig.GameProcessName.DefaultValue)
             {
                 Logger.LogWarning("This config file was written for a game named " +
                     $"{PrototyperConfig.GameProcessName.Value}, and may not work in this game.");
