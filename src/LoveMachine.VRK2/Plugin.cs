76d5115c081e3015eae244bcc54165a3a9bf91d1
Sauceke <saucekedesu@protonmail.com>
2025-08-02 21:27:30 +0300

    [VRK2] Add VR na Kanojo (wip)

---
+++ src/LoveMachine.VRK2/Plugin.cs
@@ -0,0 +1,9 @@
+﻿using BepInEx;
+using LoveMachine.Core;
+using LoveMachine.Core.NonPortable;
+
+namespace LoveMachine.VRK2;
+
+[BepInPlugin(Globals.GUID, Globals.PluginName, Globals.Version)]
+internal class Plugin : LoveMachinePlugin<VRKanojo2Game>
+{ }
---
+++ src/LoveMachine.VRK2/VRKanojo2Game.cs
@@ -0,0 +1,92 @@
+﻿using HarmonyLib;
+using Il2CppInterop.Runtime;
+using LoveMachine.Core.Common;
+using LoveMachine.Core.Game;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+namespace LoveMachine.VRK2
+{
+    public class VRKanojo2Game : GameAdapter
+    {
+        private Component uac;
+        private Il2CppSystem.Reflection.PropertyInfo loopCount;
+        private Il2CppSystem.Reflection.PropertyInfo playingClip;
+
+
+        protected override MethodInfo[] StartHMethods => new MethodInfo[]
+        {
+            AccessTools.Method("Vrk.Process.HPartProcess, Assembly-CSharp:Vrk_Process_IProcess_Init")
+        };
+
+        protected override MethodInfo[] EndHMethods => new MethodInfo[]
+        {
+            AccessTools.Method("Vrk.Process.HPartProcess, Assembly-CSharp:Vrk_Process_IProcess_InterruptProcess")
+        };
+
+        protected override Dictionary<Bone, string> FemaleBoneNames => new Dictionary<Bone, string>
+        {
+            { Bone.Vagina, "Touch_Crotch" }
+        };
+
+        protected override float MinStrokeLength => 0.25f;
+
+        protected override Transform PenisBase =>
+            GameObject.Find("Cha_Player01/Models/Cha_Player01_FP/Reference/Hips/R_Crotch1_null/R_Crotch1").transform;
+
+        protected override int AnimationLayer => throw new NotImplementedException();
+
+        protected override int HeroineCount => 1;
+
+        protected override int MaxHeroineCount => 1;
+
+        protected override bool IsHardSex => false;
+
+        protected override Animator GetFemaleAnimator(int girlIndex) => throw new NotImplementedException();
+
+        protected override void GetAnimState(int girlIndex, out float normalizedTime, out float length, out float speed)
+        {
+            try
+            {
+                normalizedTime = loopCount.GetValue(uac).Unbox<float>();
+            }
+            catch (Exception e)
+            {
+                Logger.LogDebug(e);
+                normalizedTime = 0f;
+            }
+            length = 1f;
+            speed = 1f;
+        }
+
+        protected override GameObject GetFemaleRoot(int girlIndex) => GameObject.Find("Cha01_Sakura01");
+
+        protected override string GetPose(int girlIndex)
+        {
+            try
+            {
+                return playingClip.GetValue(uac).ToString();
+            }
+            catch (Exception e)
+            {
+                Logger.LogDebug(e);
+                return "unknown";
+            }
+        }
+
+        protected override bool IsIdle(int girlIndex) => false;
+
+        protected override IEnumerator UntilReady(object instance)
+        {
+            yield return new WaitForSecondsRealtime(10f);
+            var chara = GameObject.Find(
+                "Cha01_Sakura01/Models/Cha_Sakura_BodyTop/Cha_Sakura_BodyTop/Reference");
+            var animControllerType = Il2CppType.From(
+                Type.GetType("Vrk.Interactive.UniversalAnimationController, Assembly-CSharp"));
+            uac = chara.GetComponent(animControllerType);
+            loopCount = animControllerType.GetProperty("LoopCount");
+            playingClip = animControllerType.GetProperty("PlayingClip");
+        }
+    }
+}
